ae1547278d7c6c46c9b7774a09d0d33541ae5839
Abilesh Ayyanar <abileshayyanar01@gmail.com>
2025-09-12 11:02:01 -0600

    Implemented SQLAlchemy

--- ToDoApp.py
+++ ToDoApp.py
@@ -1,29 +1,50 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 
 app = Flask(__name__)
 
-tasks = []
+# configure the SQLite database
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'ToDo.db')
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return f"<Task {self.id}: {self.content}>"
+
+# Route for the home page
 @app.route('/')
 def index():
+    tasks = Task.query.all()
     return render_template('index.html', tasks=tasks)
 
+# Route to add a new task
 @app.route('/add', methods=['POST'])
 def add():
-    task = request.form.get('task')
-    if task:
-        tasks.append(task)
+    task_content = request.form.get('task')
+    if task_content:
+        new_task = Task(content=task_content)
+        db.session.add(new_task)
+        db.session.commit()
     return redirect(url_for('index'))
 
+# Route to delete a task
 @app.route('/delete/<int:task_id>')
 def delete(task_id):
-    if 0 <= task_id < len(tasks):
-        tasks.pop(task_id)
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
     return redirect(url_for('index'))
 
+
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
     
-
-
