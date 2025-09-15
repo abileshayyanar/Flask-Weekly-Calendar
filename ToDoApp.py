d0c4eec8b93a43defecd194cab63633aba35e6c0
Abilesh Ayyanar <abileshayyanar01@gmail.com>
2025-09-15 11:34:33 -0600

    added day into Task class

--- ToDoApp.py
+++ ToDoApp.py
@@ -13,7 +13,8 @@ db = SQLAlchemy(app)
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
-
+    day = db.Column(db.string(20), nullable=False)
+    
     def __repr__(self):
         return f"<Task {self.id}: {self.content}>"
 
