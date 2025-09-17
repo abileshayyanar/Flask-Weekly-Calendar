3bd23da41a6f8ee488b9245bccae4922dbeb76bf
abileshayyanar <abileshayyanar01@gmail.com>
2025-09-16 19:45:47 -0600

    added port to deploy with Render

--- ToDoApp.py
+++ ToDoApp.py
@@ -59,6 +59,8 @@ def mark_done(task_id):
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
     with app.app_context():
         db.create_all()
     app.run(debug=True)
